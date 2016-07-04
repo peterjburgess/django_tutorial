e786416c6759e99ca2bba134ceed6eb9ed842259
peterjburgess <petebur@gmail.com>
2016-07-03 19:37:51 -0700

    Added 2 more test cases for was_published_recently()

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -15,3 +15,21 @@ class QuestionMethodTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertEqual(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() should return False for question with
+        pub_date > 1 day
+        """
+        time = timezone.now() - datetime.timedelta(days=2)
+        old_question = Question(pub_date=time)
+        self.assertEqual(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() should return True for question with
+        pub_date < 1 day
+        """
+        time = timezone.now() - datetime.timedelta(hours=20)
+        recent_question = Question(pub_date=time)
+        self.assertEqual(recent_question.was_published_recently(), True)
