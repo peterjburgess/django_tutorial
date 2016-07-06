3e68479b42870cce3e49254496949bd273184969
peterjburgess <petebur@gmail.com>
2016-07-05 22:26:15 -0700

    Added tests for detail view

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -99,3 +99,25 @@ class QuestionViewTests(TestCase):
                 ['<Question: Past question 1.>',
                  '<Question: Past question 2.>'])
 
+
+class QuestionDetailTests(TestCase):
+    def test_detail_view_with_a_future_question(self):
+        """
+        The detail view of a future question should return a 404 if 
+        pub_date is in the future.
+        """
+        future_question = create_question(question_text="Future question.", days = 1)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_detail_view_with_a_past_question(self):
+        """
+        The detail view of a past question should display the question's
+        text
+        """
+        past_question = create_question(question_text="Past question.", days = -1)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
+
